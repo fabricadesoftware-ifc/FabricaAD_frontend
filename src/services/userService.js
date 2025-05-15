d4d92412b0d1abe12c9610b07e7f88137a372803
vinyzada <vinyescolas@gmail.com>
2025-05-14 22:34:43 -0300

    feat: created function for token refresh at api.js and logout function at userService.js #4

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,22 +1,31 @@
-import api from '@/plugins/api';
+import api from "@/plugins/api";
 
 class UserLogin {
-    async login(registration, password) {
+  async login(registration, password) {
     try {
-        const {data} = await api.post('token/', {
+      const { data } = await api.post("token/", {
         registration,
-        password
-        }); 
-        const token = data.access;
-        
-        localStorage.setItem('token', token);
-        api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-        console.log('Login bem-sucedido:');
+        password,
+      });
+
+      const accessToken = data.access;
+      const refreshToken = data.refresh;
+      localStorage.setItem("access_token", accessToken);
+      localStorage.setItem("refresh_token", refreshToken);
+
+      console.log("Login bem-sucedido");
     } catch (error) {
-        console.error('Erro ao fazer login:', error);
-        throw error;
+      console.error("Erro ao fazer login:", error);
+      throw error;
     }
-}
+  }
+
+  logout() {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
+    delete api.defaults.headers.common["Authorization"];
+    console.log("Logout efetuado");
+  }
 }
 
 export default new UserLogin();
