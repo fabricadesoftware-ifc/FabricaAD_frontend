c67d16ab9307bd3a2496be3b0cf58c38894b3178
luansilva25 <luansilva250807@gmail.com>
2025-09-09 13:04:36 -0300

    REFACTOR: refactoring panel design

--- src/pages/Panel.vue
+++ src/pages/Panel.vue
@@ -3,13 +3,24 @@ import PanelTable from '@/components/panel/PanelTable.vue';
 import defaultLayout from '@/layouts/defaultLayout.vue';
 
 onMounted(() => {
-    
+
 })
 </script>
 <template>
-        <defaultLayout>
-        <div>
-            <panel-table />
+    <defaultLayout>
+        <div class="d-flex flex-column justify-space-between h-100">
+            <div class="d-flex justify-space-between">
+                <div class="w-50">
+                    <VTextField variant="outlined" placeholder="Buscar Tópico" rounded="lg" />
+                </div>
+                <VBtn color="blue" icon="mdi-plus" rounded="lg" />
+            </div>
+            <div class="pa-5">
+                <panel-table />
+            </div>
+            <div class="d-flex justify-end algin-end">
+                <VPagination :length="5" rounded="circle"/>
+            </div>
         </div>
     </defaultLayout>
 </template>
